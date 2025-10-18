484e5f958b6bbb9d68169bcad6d72da0ddb1f842
himavarsha <kolamudihimavarsha5@gmail.com>
2025-10-18 08:24:02 +0530

    Add files via upload

---
+++ First.jsx
@@ -0,0 +1,9 @@
+const First=()=>{
+
+    return(
+        <>
+        <h1> welcome to React.....</h1>
+        </>
+    )
+}
+export default First;
---
+++ comp2.jsx
@@ -0,0 +1,10 @@
+import Comp1 from "./Comp1";
+const Comp2=()=>{
+     return(
+     <>
+     <Comp1></Comp1>
+     <h1 > Welcome to Comp2......</h1>
+     </>
+
+     )
+    }
